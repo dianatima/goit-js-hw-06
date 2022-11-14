868caff1b17a3dbc341a974306fb529772399533
Yulia <nayanzina@gmail.com>
2022-11-14 20:41:41 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,6 +4,9 @@ const output = document.querySelector('#name-output');
 
 const onInput = (event) =>{
   output.textContent = event.currentTarget.value.trim();
+  if (!output.textContent) {
+    output.textContent = 'Anonymous';
+  }
 }; 
 
 input.addEventListener('input', onInput);
