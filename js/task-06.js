dc6f263bbe026f607ec1916b0b20e08766af61df
Yulia <nayanzina@gmail.com>
2022-11-14 20:42:18 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,13 @@
 const input = document.querySelector('#validation-input');
 
 const onInputBlur = (event) => {
-    event.currentTarget.value.trim().length === Number(input.dataset.length) ? input.classList.add('valid') : input.classList.add('invalid');  
+    if (event.currentTarget.value.trim().length === Number(input.dataset.length)) {
+        input.classList.remove('invalid');
+        input.classList.add('valid');
+    } else {
+        input.classList.remove('valid');
+        input.classList.add('invalid'); 
+    };
 };
 
 input.addEventListener('blur', onInputBlur);
